464b8ea16d49dac9eece0fb48aa85166c5d17c42
khrisplayedit <onyedikachionwuchekwa@yahoo.com>
2023-03-02 23:02:23 +0100

    first commit

---
+++ gradie.js
@@ -0,0 +1,10 @@
+let username;
+document.getElementById("button").onclick = function()
+{
+username = document.getElementById("place1").value;
+
+if(username == username)
+
+alert("welcome," +  username)
+
+}
